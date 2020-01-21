59d8d0f882329700d22edab759ce6638f9becec6
dbills777 <dbills777@gmail.com>
2020-01-21 11:26:56 -0700

    added third verb to nonsense story

--- u6/index.js
+++ u6/index.js
@@ -23,7 +23,7 @@ function tellStory() {
     .value.toLowerCase()
     .split(/\s*[,\n." "]+\s*/);
   console.log(verbArray);
-  const theStory = `One day a <span>${adjectivesArray[0]}</span> <span>${nounArray[0]}</span> was <span>${verbArray[0]}ing</span> down the river. The river had <span>${nounArray[1]}'s</span>, <span>${nounArray[2]}'s</span> and <span>${nounArray[3]}'s</span> in it. Around the <span>${adjectivesArray[1]}</span> corner, <span>${nounArray[0]}</span> saw a <span>${nounArray[4]}</span> that was <span>${verbArray[1]}ing</span> a <span>${adjectivesArray[2]}</span> <span>${nounArray[5]}</span>. That was the last time a <span>${nounArray[6]}</span> was sighted.`;
+  const theStory = `One day a <span>${adjectivesArray[0]}</span> <span>${nounArray[0]}</span> was <span>${verbArray[0]}ing</span> down the river. The river had <span>${nounArray[1]}'s</span>, <span>${nounArray[2]}'s</span> and <span>${nounArray[3]}'s</span> in it. Around the <span>${adjectivesArray[1]}</span> corner, <span>${nounArray[0]}</span> saw a <span>${nounArray[4]}</span> that was <span>${verbArray[1]}ing</span> a <span>${adjectivesArray[2]}</span> <span>${nounArray[5]}</span>. That was the last time a <span>${verbArray[1]}ing </span><span>${nounArray[6]}</span> was sighted.`;
   const output = document.querySelector("#result");
   output.innerHTML = theStory;
 }
