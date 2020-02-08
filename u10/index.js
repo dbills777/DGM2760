981389b0ac9c1b47bc50cfde48a2e5aae37bb3cf
dbills777 <dbills777@gmail.com>
2020-02-07 18:26:33 -0700

    u10 setup

--- u10/index.js
+++ u10/index.js
@@ -1,21 +1,5 @@
-function duplicateMenu() {
-  //get all the anchor elements from the top menu
-  topList = document.querySelectorAll("ul#primaryNavigation li a");
-  console.log(topList);
-  // can check inspect tools to find all available properties of a NodeLIst
-
-  //create the new list items for the bottom of the page
-  topList.forEach(menuItem => {
-    let newUl = document.createElement("ul");
-    let newLi = document.createElement("li");
-    let newLink = document.createElement("a");
-    newLink.setAttribute("href", menuItem.getAttribute("href"));
-    newLi.appendChild(newLink);
-    document.querySelector("#smallNavArea").appendChild(newUl);
-    newUl.appendChild(newLi);
-    console.log(menuItem.text);
-    newLink.textContent = menuItem.innerText;
-  });
-}
-
-duplicateMenu();
+//set the company name
+document.querySelector("#companyName").textContent = "Hotel California";
+//set the company slogan
+document.querySelector("#companySlogan").textContent =
+  "Check Out Available Rooms";
