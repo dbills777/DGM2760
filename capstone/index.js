6bad7a49c39bddd40a05f691bfe068d6fc6660da
dbills777 <dbills777@gmail.com>
2020-04-07 20:46:32 -0600

    display movies

--- capstone/index.js
+++ capstone/index.js
@@ -4,16 +4,19 @@ const IMAGE_URL = "https://image.tmdb.org/t/p/w500";
 const url =
   "https://api.themoviedb.org/3/search/movie?api_key=f757473356258e461984b8a22062f5a4";
 
+// selecting elements from the dom
 const searchButton = document.querySelector("#search");
 const inputElement = document.querySelector("#inputValue");
 const movieSearchable = document.querySelector("#movie-searchable");
+// const imgElement = document.querySelector('img')
 
 function movieSection(movies) {
   return movies.map((movie) => {
-    return `
-    <h1>${movie.title}</h1>
-    <img src=${IMAGE_URL + movie.poster_path} data-movie-id=${movie.id}/>
-        `;
+    if (movie.poster_path) {
+      return `<div><h1>${movie.title}</h1><img src=${
+        IMAGE_URL + movie.poster_path
+      } data-movie-id=${movie.id}/></div>`;
+    }
   });
 }
 
@@ -33,6 +36,16 @@ function createMovieCards(movies) {
   return movieElement;
 }
 
+function renderSearchMovies(data) {
+  //data.results[]
+  movieSearchable.innerHTML = "";
+  const movies = data.results;
+  console.log(movies);
+  const movieBlock = createMovieCards(movies);
+  movieSearchable.appendChild(movieBlock);
+  console.log("Data: ", data);
+}
+
 searchButton.onclick = function (event) {
   event.preventDefault();
   const value = inputElement.value;
@@ -42,16 +55,22 @@ searchButton.onclick = function (event) {
 
   fetch(searchURL)
     .then((res) => res.json())
-    .then((data) => {
-      //data.results[]
-      const movies = data.results;
-      console.log(movies);
-      const movieBlock = createMovieCards(movies);
-      movieSearchable.appendChild(movieBlock);
-      console.log("Data: ", data);
-    })
+    .then(renderSearchMovies)
     .catch((error) => {
       console.log("Error", error);
     });
+  inputElement.value = "";
   console.log("Value:", value);
+};
+
+// Event Delegation
+document.onclick = function (event) {
+  const target = event.target;
+  if (target.tagName.toLowerCase() === "img") {
+    console.log("hello world");
+    const section = event.target.parentElement.parentElement; // section
+    const content = section.nextElementSibling; // content
+    content.classList.add('content-display');
+    console.log(content)
+  }
 };
